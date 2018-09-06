3ddc7a9170047b3749191186a50006dcae486617
zAwAt <43028376+zAwAt@users.noreply.github.com>
2018-09-06 22:07:00 +0900

    Add files via upload

---
+++ setvelocity.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+def main():
+    pub=rospy.Publisher("/mobile_base/commands/velocity",Twist,queue_size=10)
+    vel_msg=Twist()
+    vel_msg.linear.x=0
+    vel_msg.linear.y=0
+    vel_msg.linear.z=0
+    vel_msg.angular.x=0
+    vel_msg.angular.y=0
+    vel_msg.angular.z=0
+    while True :
+        pub.publish(vel_msg)
+
+    rospy.spin()
+
+if __name__== "__main__":
+    try:
+        rospy.init_node("SetVelocity")
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
